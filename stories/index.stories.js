cc77c826efd892ddeb001c5cff93837e5aaf38d5
AVIRAMIN <aviram.iny@amdocs.com>
2019-08-07 15:10:13 +0300

    added button + knob test + scss

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,18 +5,31 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { withInfo } from '@storybook/addon-info';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo';
+import Button from '../src/components/form/Button'
+import { text, boolean, number } from '@storybook/addon-knobs/react';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
+// storiesOf('Button', module)
+//   .add('with text', withInfo('This is an informational paragraph you can use to describe your component')(
+//     () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   )
+//   .add('with some emoji', () => (
+//     <Button onClick={action('clicked')}>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍 💯
+//       </span>
+//     </Button>
+//   ));
+
+// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
 storiesOf('Button', module)
-  .add('with text', withInfo('This is an informational paragraph you can use to describe your component')(
-    () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  )
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+  .add('primary', withInfo('This is an informational paragraph you can use to describe your component')(
+    () => <Button onClick={action('clicked')} type={text('type', 'primary')} >Primary Button</Button>))
+  .add('secondary', withInfo('This is an informational paragraph you can use to describe your component')(
+    () => <Button onClick={action('clicked')} type='secondary'>Secondary Button</Button>))
+  .add('primary-negative', withInfo('This is an informational paragraph you can use to describe your component')(
+    () => <Button onClick={action('clicked')} type='primary-negative'>Secondary Button</Button>));
+
